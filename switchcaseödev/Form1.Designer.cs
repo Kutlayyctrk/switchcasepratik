7252963443a02ce4c36c2e3162561d61ffe84fa2
Kutlayyctrk <ktlyyctrk@gmail.com>
2023-07-11 02:53:31 +0300

    Add project files.

---
+++ switchcaseödev/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace switchcaseödev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(43, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 18);
+            label1.TabIndex = 0;
+            label1.Text = "Sayı1:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(43, 126);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 18);
+            label2.TabIndex = 1;
+            label2.Text = "Sayi2:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(148, 56);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 25);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(148, 119);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 25);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(148, 177);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 25);
+            textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(157, 250);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 6;
+            button1.Text = "Hesapla";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(362, 126);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 18);
+            label4.TabIndex = 7;
+            label4.Text = "Sonuç:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(441, 123);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 25);
+            textBox4.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(42, 184);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 18);
+            label3.TabIndex = 2;
+            label3.Text = "İşlem:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 18F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Orange;
+            ClientSize = new Size(705, 538);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Arial", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Ödev";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label3;
+    }
+}
